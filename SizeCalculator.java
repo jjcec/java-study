74529a62f6dca0051afae3908c5c4d513a41c1a7
jjcec <jjcec@naver.com>
2017-11-05 13:24:25 +0900

    장판가격 계산
    장판의 폭과 길이 그리고 단위 면적당 가격을 알았을때 장판의 가격을 계산하는 (SizeCalculator.java) 프로그램을 작성하세요. 장판의 폭은 10m 이고, 장판의 길이는 20m 입니다. 장판의 단위면적 (1m2) 의 가격은 10000원 입니다. 장판의 폭, 길이, 단위면적 을 변수에 저장 후 최종 가격을 구해서 화면에 출력하세요.

---
+++ SizeCalculator.java
@@ -0,0 +1,13 @@
+
+public class SizeCalculator {
+    public static void main(String[] args) {
+    	int width = 10;
+    	int length = 20;
+    	int area = width*length;
+    	int price = area*10000;
+    	
+    	System.out.println("최종가격: " + price);
+    	
+    	
+    }
+}
