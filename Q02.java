7d7a66f961c660b5d37ffd222548446cff14dbb8
jjcec <jjcec@naver.com>
2017-11-05 13:34:44 +0900

    실습2
    X 라는 변수에는 4.5 (실수형태) Y 라는 변수에는 2 (정수형태) 를 저장후 다음 결과값을 출력하세요.
    	a. x/y 
    b. y / x 
    c. x%y

---
+++ Q02.java
@@ -0,0 +1,14 @@
+
+public class Q02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        double X = 4.5;
+        int Y = 2;
+        
+        System.out.println("x/y = " +X/Y);
+        System.out.println("y/x = " +Y/X);
+        System.out.println("x%y = " +X%Y);
+	}
+
+}
