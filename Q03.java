7bd1a2e9a9026dd41678b1f83f3de002b82ea138
jjcec <jjcec@naver.com>
2017-11-05 13:37:36 +0900

    실습3
    1 부터 5까지의 정수를 각각 다른 5개의 변수에 대입 후 해당 값이 3의 배수라면 true 를 출력하고, 3의 배수가 아니라면 false 를 출력하는 프로그램을 작성하세요 (if, for, while, switch 문 사용금지).

---
+++ Q03.java
@@ -0,0 +1,25 @@
+
+public class Q03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        
+        boolean aa = a%3 == 0;
+        boolean bb = b%3 == 0;
+        boolean cc = c%3 == 0;
+        boolean dd = d%3 == 0;
+        boolean ee = e%3 == 0;
+        
+        System.out.println(aa);
+        System.out.println(bb);
+        System.out.println(cc);
+        System.out.println(dd);
+        System.out.println(ee);
+	}
+
+}
