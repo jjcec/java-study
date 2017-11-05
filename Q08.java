1e103df97c428654fb6a6f9bc9f330b8e706f93d
jjcec <jjcec@naver.com>
2017-11-05 13:41:28 +0900

    실습8
    아래 모양의 도형을 for 문을 이용해서 완성하세요. 전체 높이는 10층이며, 각 층마다 * 표시가 하나씩 감소합니다.

---
+++ Q08.java
@@ -0,0 +1,13 @@
+public class Q08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 10; i>0 ; i--){
+	        for(int j =0;j<i;j++){
+	        	System.out.print('*');
+	        }
+	        	System.out.println();
+	     }
+	}
+
+}
