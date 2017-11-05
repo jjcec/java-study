52c4aefdd972fca67b8d6035cdfec4577e3e7edb
jjcec <jjcec@naver.com>
2017-11-05 13:54:08 +0900

    실습14
    3행 4열의 int  타입의 2차원 배열을 선언 후, 1행은 85, 80,70,90 으로 2행은 70,100,90,60으로 3행은 100,100, 80,75 로 초기화 한 후 (선언과 초기화를 같이해도 됩니다), 각 행의 평균값과, 각 열의 평균값을 구해서 출력하는 프로그램을 작성하세요. (결과물 예시: 1행의 평균값: XXX, 2행의 평균값: YYY, 3행의 평균값: ZZZ, 1열의 평균값: AAA, …..)

--- Q09.java
+++ Q09.java
@@ -3,41 +3,25 @@ public class Q09 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        //int num = (int) (Math.random()*6)+1;
-        int sum1 = 0;
-        int sum2 = 0;
-        int sum3 = 0;
-        int sum4 = 0;
-        int sum5 = 0;
-        int sum6 = 0;
-        
-        for(int i =0;i<10000;i++){
-        	int num = (int) (Math.random()*6)+1;
-            if(num == 1){
-            	sum1 += 1;
-            }
-            else if(num == 2){
-            	sum2 += 1;
-            }
-            else if(num == 3){
-            	sum3 += 1;
-            }
-            else if(num == 4){
-            	sum4 += 1;
-            }
-            else if(num == 5){
-            	sum5 += 1;
-            }
-            else{
-            	sum6 += 1;
-            }
-	    }
-        System.out.println("sum1 = " + sum1);
-        System.out.println("sum2 = " + sum2);
-        System.out.println("sum3 = " + sum3);
-        System.out.println("sum4 = " + sum4);
-        System.out.println("sum5 = " + sum5);
-        System.out.println("sum6 = " + sum6);
+		int[][] threeFourArr = {{85, 80,70,90},{70,100,90,60},{100,100, 80,75}};
+		int sum = 0;
+		for(int i=0;i<threeFourArr.length; i++){
+        	for(int j =0; j<threeFourArr[i].length; j++){
+        		sum += threeFourArr[i][j];
+        	}
+        	double average  = (double)sum/threeFourArr[i].length;
+        	System.out.println(i+1 +"ÇàÀÇ Æò±Õ°ª: " + average);
+        	sum = 0;
+        }
+		//
+		for(int i=0;i<threeFourArr[0].length; i++){
+        	for(int j =0; j<threeFourArr.length; j++){
+        		sum += threeFourArr[j][i];
+        	}
+        	double average  = (double)sum/threeFourArr.length;
+        	System.out.println(i+1 +"¿­ÀÇ Æò±Õ°ª: " + average);
+        	sum = 0;
+        }
 	}
 
 }
