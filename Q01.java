394d75efe60cf961dd27515c7a7994badd84feb0
jjcec <jjcec@naver.com>
2017-11-05 13:31:53 +0900

    실습1
    X 라는 변수에는 5를 대입하고, Y 라는 변수에는 9를 대입했을때 아래 연산의 결과값을 보여주는 프로그램을 작성하세요.
    a. x + y * 2 
    b. x - y + 2 
    c. (x+y) * 2 
    d. y % x

---
+++ Q01.java
@@ -0,0 +1,16 @@
+
+public class Q01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int X = 5;
+        int Y = 9;
+        
+        System.out.println("x + y * 2 =" + (X + Y * 2) );
+        System.out.println("x - y + 2 =" + (X - Y + 2) );
+        System.out.println("(x+y) * 2 =" + ((X+Y) * 2) );
+        System.out.println("y%x =" + (Y%X) );
+        
+	}
+
+}
