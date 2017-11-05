6505fb5b6462e3aaeb38c2fe63b06dc9ef3524fe
jjcec <jjcec@naver.com>
2017-11-05 13:40:50 +0900

    실습7
    아래 모양의 도형을 for 문을 이용해서 완성하세요. 전체 높이는 10층이며, 각 층마다 * 표시가 하나씩 증가합니다. 힌트 System.out.println() 에서 출력되는 문장은 줄바꿈이 생기지만, System.out.print() 로 인쇄하는 문자는 줄바꿈 없이 화면에 보여집니다

---
+++ Q07.java
@@ -0,0 +1,14 @@
+
+public class Q07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        for(int i = 1; i<11 ; i++){
+        	for(int j =0;j<i;j++){
+        	System.out.print('*');
+        }
+        	System.out.println();
+        }
+	}
+
+}
