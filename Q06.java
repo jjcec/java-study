3151468239cfdbf8da699a939ecd369f79335dd9
jjcec <jjcec@naver.com>
2017-11-05 13:39:53 +0900

    실습6
    score 라는 int 변수를 선언 후 해당 변수 값이 100, 99, 98, 97, 96 점일 경우에는 grade 변수에 ”A+”라는 문자열을, 95, 94, 93, 92, 91, 90 점일때는 ”A”라는 문자열을, 나머지 점수에는 ”B+” 라는 문자열을 switch 문을 이용해서 완성하세요.

---
+++ Q06.java
@@ -0,0 +1,31 @@
+
+public class Q06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score = 95;
+		char grade;
+		
+		switch(score){
+		case 100:
+		case 99:
+		case 98:
+		case 97:
+		case 96:
+			System.out.println("A+");
+			break;
+		case 95:
+		case 94:
+		case 93:
+		case 92:
+		case 91:
+		case 90:
+			System.out.println("A");
+		    break;
+		default:
+			System.out.println("B+");
+		
+		}
+	}
+
+}
