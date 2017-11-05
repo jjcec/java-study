17c717d93f7d8fb128b0de01dfd6e0df7911852a
jjcec <jjcec@naver.com>
2017-11-05 14:04:24 +0900

    실습13
    7. 3개의 String 변수를 저장하는 배열을 splitOutput 이라는 변수명으로 선언 한 후, input 이라는 String 변수를 “Object Oriented Programming,Open Source Software Programming,BigData Analysis” 라는 값으로 선언 해준후, input 변수를 쉼표(,)를 기준으로 나누어서 splitOutput 이라는 변수에 저장해주세요.
    
    8. 7번 문제에서 만들어진 splitOutput 이라는 배열을 for loop 를 이용하여 각 변수의 값을 읽어들인 후, 각 원소의 값이 “Object Oriented Programming” 이라는 값과 일치하는지 출력하고, 각 String 원소의 문자열 길이를 출력하고, “Programming” 이라는 문자열이 시작하는 위치의 인덱스를 출력하는 프로그램을 작성하세요.

---
+++ Q13.java
@@ -0,0 +1,48 @@
+
+public class Q13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Q07
+
+		String[] splitOutput = new String[3];
+		String input = "Object Oriented Programming,Open Source Software Programming,BigData Analysis";
+		splitOutput[0]="Object Oriented Programming";
+		splitOutput[1]="Open Source Software Programming";
+		splitOutput[2]="BigData Analysis";
+		
+		/*
+		String splitOutput = new Stirng[3];
+		String splitOutput = "Object Oriented Programming,Open Source Software Programming,BigData Analysis";
+		String[] ids = splitOutput.split(",");
+		for(String id : ids){
+			System.out.println(id);
+		}
+		*/
+		
+		//Q08
+		System.out.println("각 원소의 값이 “Object Oriented Programming” 이라는 값과 일치하는가");
+		for(int i =0; i<splitOutput.length;i++){
+			if(splitOutput[i] == "Object Oriented Programming"){
+				System.out.println("일치");
+			}
+			else
+				System.out.println("불일치");
+		}
+		System.out.println(" ");
+		//
+		System.out.println("각 String 원소의 문자열 길이");
+		for(int i =0; i<splitOutput.length;i++){
+			
+			System.out.println(splitOutput[i].length());
+		}
+		System.out.println(" ");
+		//
+		System.out.println("“Programming” 이라는 문자열이 시작하는 위치의 인덱스");
+		for(int i = 0;i<splitOutput.length;i++){
+			int index0fProgram = splitOutput[i].indexOf("Programming");
+	        System.out.println(index0fProgram);
+		}
+	}
+
+}
