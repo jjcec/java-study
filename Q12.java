a6be529be804730056b52927f5036387df9cff62
jjcec <jjcec@naver.com>
2017-11-05 14:02:51 +0900

    실습12
    4. 10개의 정수를 저장할 수 있는 배열을 선언 한 후, 배열안 각원소의 초기값을 각각의 인덱스 값 (0 부터 9까지) 로 초기화 하는 코드를 작성하세요.
    
    5. 앞선 4번 문제에서 초기화 시킨 배열의 값들의 위치를 임의로 변경 (random shuffling, 무작위로 섞어서 다시 재배치) 한 후 재배치된 배열의 값들을 출력하는 프로그램을 작성하세요. 힌트: Math.random() 을 이용할 수 있으며, 같은 값이 중복되어서 나와도 상관없습니다.
    
    6. 100 개의 정수를 저장할 수 있는 배열을 선언해 주세요 (input).  Math.random() 함수를 이용해서 1~10 까지의 임의의 숫자를 발생시켜서 100개의 정수 배열을 초기화 해주세요 (100 개의 배열 원소들은 1~10까지 임의의 숫자중 하나를 초기값으로 가지게 됩니다). 해당 정수 배열의 값들을 읽어 들인 후, 각각의 숫자가 몇번 나왔는지, 또다른 배열에 저장 한 후 (output), 그 배열의 값을 출력해주는 프로그램을 작성하세요.

---
+++ Q12.java
@@ -0,0 +1,54 @@
+
+public class Q12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Q04
+		int[] arr4 = new int[10];
+		for (int i=0; i < arr4.length ; i++ ) { 
+           arr4[i] = i;
+		}
+        /*
+		arr4[0]=0;
+		arr4[1]=1;
+		arr4[2]=2;
+		arr4[3]=3;
+		arr4[4]=4;
+		arr4[5]=5;
+		arr4[6]=6;
+		arr4[7]=7;
+		arr4[8]=8;
+		arr4[9]=9;
+		*/
+		System.out.println("Q4 결과값");
+		for (int i =0;i<arr4.length;i++){
+        	System.out.println("arr4[" + i + "] : " + arr4[i]);
+        }
+		System.out.println("");
+		
+		//Q05
+		System.out.println("Q5 결과값");
+		for(int i =0;i<arr4.length;i++){
+			arr4[i] = (int) (Math.random()*10);
+			System.out.println("arr4[" + i + "] : " + arr4[i]);
+		}
+		System.out.println("");
+		
+		//Q06
+		System.out.println("Q6 결과값");
+		int arr6[] = new int[100];
+		for(int i =0;i<arr6.length;i++){
+			arr6[i] = (int) (Math.random()*10+1);
+		}
+		
+		int num_arr6[] = new int[10];
+		for(int i =0;i<arr6.length;i++){
+			num_arr6[arr6[i]-1]++;
+		}
+		for (int i=0; i < num_arr6.length ; i++ ) { 
+            System.out.println( i+1 +"의 개수 :"+ num_arr6[i]); 
+      } 
+		
+	}
+
+}
