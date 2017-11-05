99e498a67ce090b34a62ea31662382a08c338716
jjcec <jjcec@naver.com>
2017-11-05 14:00:48 +0900

    실습11
    1. scores 라는 배열을 선언후, 해당 배열의 값을 90, 85, 100, 70, 95 로 입력한 후, 해당 배열의 인덱스 값을 이용하여, 모든 배열의 값을 출력하는 프로그램을 작성하세요
    
    2. 1번문제에서 생성한 배열을 이용하여, 각 원소의 인덱스 값으로 각각 인자를 접근해서 해당 배열의 최대값, 최소값을 구해내는 프로그램을 작성하세요.
    
    3. 1번문제에서 생성한 배열을 이용하여, 각 원소의 인덱스 값을 이용하지 않고 (세가지 항 - 초기값, 컨디션, 코드블록 이후 실행문 - 없는 for loop), 해당 배열 값들의 총합 및 평균값을 구하는 프로그램을 작성하세요.

---
+++ Q11.java
@@ -0,0 +1,43 @@
+public class Q11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Q01
+		int[] scores = {90,85,100,70,95};
+		
+		System.out.println("Q1 결과값");
+		System.out.println("scores[0]: " + scores[0]);
+		System.out.println("scores[1]: " + scores[1]);
+		System.out.println("scores[2]: " + scores[2]);
+		System.out.println("scores[3]: " + scores[3]);
+		System.out.println("scores[4]: " + scores[4]);
+		System.out.println(" ");
+		
+		//Q02
+		int max= scores[0];
+		int min=scores[0];
+		for(int i=1;i<scores.length;i++){
+			if(scores[i]>max)
+				max = scores[i];
+			else if(scores[i]<min)
+				min = scores[i];
+		}
+		System.out.println("Q2 결과값");
+		System.out.println("max: " +max);
+		System.out.println("min: " +min);
+		System.out.println(" ");
+		
+		//Q03
+		int sum=0;
+		for(int s : scores){
+			sum += s;
+		}
+		System.out.println("Q3 결과값");
+		System.out.println("총합: " + sum);
+		double average = (double)sum/scores.length;
+		System.out.println("평균값: " + average);
+
+	}
+
+}
