4973fa5d020c2715563a1c063a6d3821879dac20
jjcec <jjcec@naver.com>
2017-11-05 13:22:32 +0900

    정보출력하기

---
+++ StudentInfo.java
@@ -0,0 +1,11 @@
+
+public class StudentInfo {
+    public static void main(String[] args) {
+    	System.out.println("이름: 홍길동");
+    	System.out.println("ID: 123456");
+    	System.out.println("Slack ID: abcd");
+    	System.out.println("GitHub ID: abcde");
+    	System.out.println("주소: 서울시 서대문구");
+    	System.out.println("전화번호: 010-1234-5678");
+    }
+}
