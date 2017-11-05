de2edb0af92d0593b475431fda04ab9c3f9bb2cf
jjcec <jjcec@naver.com>
2017-11-05 13:38:14 +0900

    실습4
    seconds 라는 이름을 가지는 변수를 선언 후 102343 이라는 값을 저장하세요. 해당 변수값은 초를 나타내는 값입니다. 0초를 기준으로 했을때 해당 초값이 표현하는 값이 몇일, 몇시, 몇분, 몇초를 나타내는지를 day, hour, minute, sec 이라는 변수에 저장해서 출력하세요 (if, for, while, switch 문 사용금지).

---
+++ Q04.java
@@ -0,0 +1,18 @@
+
+public class Q04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        float seconds = 102343;
+        float sec = seconds;
+        float minute = seconds/60;
+        float hour = minute/60;
+        float day = hour/24;
+        
+        System.out.println("sec = " + sec);
+        System.out.println("minute = " + minute);
+        System.out.println("hour = " + hour);
+        System.out.println("day = " + day);
+	}
+
+}
